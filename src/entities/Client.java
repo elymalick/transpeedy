49e298e97507da40f73f08cdc3e38c89ad3a5ae1
elymalick <elymalick@gmail.com>
2016-10-19 17:14:36 +0200

    "add entities class"

--- src/entities/Client.java
+++ src/entities/Client.java
@@ -1,5 +1,76 @@
 package entities;
 
-public class Client {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Client implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue
+	private int IdClient;
+	
+	private String nom;
+	private String prenom	;
+	private String email	;
+	private String identifiant	;
+	private String motdepasse	;
+	private String photo	;
+	private String IdAdresse	;
+	public int getIdClient() {
+		return IdClient;
+	}
+	public void setIdClient(int idClient) {
+		IdClient = idClient;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getIdentifiant() {
+		return identifiant;
+	}
+	public void setIdentifiant(String identifiant) {
+		this.identifiant = identifiant;
+	}
+	public String getMotdepasse() {
+		return motdepasse;
+	}
+	public void setMotdepasse(String motdepasse) {
+		this.motdepasse = motdepasse;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	public String getIdAdresse() {
+		return IdAdresse;
+	}
+	public void setIdAdresse(String idAdresse) {
+		IdAdresse = idAdresse;
+	}
+	
 }
